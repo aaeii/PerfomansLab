2f96d820f29a5a06fc5dd27bc9af143b8ae9c24f
aaeii <ahm-emily@yandex.ru>
2025-08-29 14:26:36 +0300

    task1

---
+++ task1/src/main/java/org/example/Task1.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Массив 1:");
+        int n1 = in.nextInt(), m1 = in.nextInt();
+        System.out.println("Массив 2:");
+        int n2 = in.nextInt(), m2 = in.nextInt();
+        int[] arr1 = new int[n1];
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = i + 1;
+        }
+        int[] arr2 = new int[n2];
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = i + 1;
+        }
+        path(n1,m1);
+        path(n2,m2);
+
+    }
+    static void path(int n, int m){
+        int i = 1;
+        do {
+            System.out.print(i);
+            i = 1 + (i + m - 2) % n;
+
+        } while (i != 1);
+    }
+}
